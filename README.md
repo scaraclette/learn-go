511b3f6c668e7cc0323839eae86512e6c65875c2
scaraclette <gustiscarlett@gmail.com>
2021-09-20 22:32:23 -0700

    Monday

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # learn-go
 Go tutorial
+
+## Notes
+- ```$ go run .```: run code
+- ```$ go mod tidy```: cleans up unused dependencies or adds missing dependencies.
+
+## TODO
+1. Go executable
---
+++ hello-1/hello.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Deno", "Scarlett", "Adrian"}
+
+	// Request a greeting message
+	// message, err := greetings.Hello("Deno")
+	messages, err := greetings.Hellos(names)
+	// If an error was returned, print it to the
+	// console and exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
+}
